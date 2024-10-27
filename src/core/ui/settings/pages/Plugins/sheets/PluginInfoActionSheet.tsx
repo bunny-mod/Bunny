879ec3c88a0c576342c1a801ec9c4188e2fdd46c
pylixonly <82711525+pylixonly@users.noreply.github.com>
2024-10-27 18:33:20 +0800

    feat(plugins): Implement simple plugin actions section

--- src/core/ui/settings/pages/Plugins/sheets/PluginInfoActionSheet.tsx
+++ src/core/ui/settings/pages/Plugins/sheets/PluginInfoActionSheet.tsx
@@ -1,6 +1,13 @@
+import { formatString, Strings } from "@core/i18n";
+import { showConfirmationAlert } from "@core/vendetta/alerts";
+import { isPluginEnabled, refreshPlugin, startPlugin, stopPlugin, uninstallPlugin } from "@lib/addons/plugins";
 import { findAssetId } from "@lib/api/assets";
+import { purgeStorage } from "@lib/api/storage";
+import { openAlert } from "@lib/ui/alerts";
+import { AlertActionButton } from "@lib/ui/components/wrappers";
 import { hideSheet } from "@lib/ui/sheets";
-import { ActionSheet, Button, Text } from "@metro/common/components";
+import { showToast } from "@lib/ui/toasts";
+import { ActionSheet, AlertActions, AlertModal, Button, Stack, TableRowGroup, Text } from "@metro/common/components";
 import { ScrollView, View } from "react-native";
 
 import { PluginInfoActionSheetProps } from "./common";
@@ -35,11 +42,93 @@ export default function PluginInfoActionSheet({ plugin, navigation }: PluginInfo
                     />}
                 </View>
             </View>
-            <View style={{ flexDirection: "row", justifyContent: "center", alignContent: "center" }}>
-                {/* <Text variant="text-lg/medium">
-                    Oops, you shouldn't see this!
-                </Text> */}
-            </View>
+            <TableRowGroup title="Actions">
+                <Stack>
+                    <ScrollView
+                        horizontal={true}
+                        contentContainerStyle={{ gap: 4 }}
+                    >
+                        <Button
+                            size="md"
+                            variant="secondary"
+                            text={Strings.REFETCH}
+                            icon={findAssetId("RetryIcon")}
+                            onPress={async () => {
+                                try {
+                                    await refreshPlugin(plugin.id);
+                                    showToast(Strings.PLUGIN_REFETCH_SUCCESSFUL, findAssetId("toast_image_saved"));
+                                } catch {
+                                    showToast(Strings.PLUGIN_REFETCH_FAILED, findAssetId("Small"));
+                                }
+                            }}
+                        />
+                        <Button
+                            size="md"
+                            variant="destructive"
+                            text={Strings.CLEAR_DATA}
+                            icon={findAssetId("CopyIcon")}
+                            onPress={() => {
+                                hideSheet("PluginInfoActionSheet");
+                                openAlert("bunny-plugin-clear-data", <AlertModal
+                                    title={Strings.HOLD_UP}
+                                    content={formatString("ARE_YOU_SURE_TO_CLEAR_PLUGIN_DATA", { name: plugin.name })}
+                                    actions={<AlertActions>
+                                        <AlertActionButton
+                                            text={Strings.CLEAR_DATA}
+                                            variant="destructive"
+                                            onPress={async () => {
+                                                if (isPluginEnabled(plugin.id)) stopPlugin(plugin.id);
+
+                                                let message: [any, any];
+                                                try {
+                                                    purgeStorage(`plugins/storage/${plugin.id}.json`);
+                                                    message = ["CLEAR_DATA_SUCCESSFUL", "trash"];
+                                                } catch {
+                                                    message = ["CLEAR_DATA_FAILED", "Small"];
+                                                }
+
+                                                showToast(
+                                                    formatString(message[0], { name: plugin.name }),
+                                                    findAssetId(message[1])
+                                                );
+
+                                                if (isPluginEnabled(plugin.id)) await startPlugin(plugin.id);
+                                            }}
+                                        />
+                                        <AlertActionButton
+                                            text={Strings.CANCEL}
+                                            variant="secondary"
+                                            onPress={() => hideSheet("PluginInfoActionSheet")}
+                                        />
+                                    </AlertActions>}
+                                />);
+
+                            }}
+                        />
+                        <Button
+                            size="md"
+                            variant="destructive"
+                            text={Strings.DELETE}
+                            icon={findAssetId("TrashIcon")}
+                            onPress={() => showConfirmationAlert({
+                                title: Strings.HOLD_UP,
+                                content: formatString("ARE_YOU_SURE_TO_DELETE_PLUGIN", { name: plugin.name }),
+                                confirmText: Strings.DELETE,
+                                cancelText: Strings.CANCEL,
+                                confirmColor: "red",
+                                onConfirm: async () => {
+                                    try {
+                                        await uninstallPlugin(plugin.id);
+                                    } catch (e) {
+                                        showToast(String(e), findAssetId("Small"));
+                                    }
+                                    hideSheet("PluginInfoActionSheet");
+                                }
+                            })}
+                        />
+                    </ScrollView>
+                </Stack>
+            </TableRowGroup>
         </ScrollView>
     </ActionSheet>;
 }
