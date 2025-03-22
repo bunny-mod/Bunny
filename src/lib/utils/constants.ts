934d1134f49cd8851e8b14cd26d599abae451299
Revolution <tavinho.daniel@gmail.com>
2025-03-22 04:25:26 -0300

    refactor: update API structure and improve code formatting

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -1,11 +1,14 @@
-export const DISCORD_SERVER = "https://discord.gg/XjYgWXHb9Q";
+export const DISCORD_SERVER = "https://discord.gg/m47ymDb9aS";
 export const GITHUB = "https://github.com/fastcord";
-export const HTTP_REGEX = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/;
-export const HTTP_REGEX_MULTI = /https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&//=]*)/g;
+export const HTTP_REGEX =
+	/^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/;
+export const HTTP_REGEX_MULTI =
+	/https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&//=]*)/g;
 
 export const FASTCORD_PROXY_PREFIX = "https://bn-plugins.github.io/vd-proxy";
 
-export const OFFICIAL_PLUGINS_REPO_URL = "https://bn-plugins.github.io/dist/repo.json";
+export const OFFICIAL_PLUGINS_REPO_URL =
+	"https://bn-plugins.github.io/dist/repo.json";
 
 export const VD_PROXY_PREFIX = "https://vd-plugins.github.io/proxy";
 export const VD_DISCORD_SERVER_ID = "1015931589865246730";
